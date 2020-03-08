1cb642d5a6b49ddf1abe8bd2527cfc7f41cbd4e7
Temir <gggvar@gmail.com>
2020-03-08 15:52:12 +0300

    zadanie 2 with two more types of cycles

--- script.js
+++ script.js
@@ -24,6 +24,35 @@ for (let i = 0; i < 2; i++){
    
 };
 
+// let i = 0;
+// while (i < 2){
+//     let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+//         b = prompt("Во сколько обойдется?",'');
+
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else{
+//         console.log("Suka blet");
+//     }
+//     i++;
+// }
+
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+//         b = prompt("Во сколько обойдется?",'');
+
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     } else{
+//         console.log("Suka blet");
+//     }
+//     i++;
+// }
+// while (i < 2);
+
+
 appData.moneyPerDay = appData.budjet / 30;
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
